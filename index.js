2d7dec8d1faafa3e05f3d22696c79757b7f24a23
Daniel Allen <danielkentonallen@gmail.com>
2021-06-28 18:12:18 -0600

    added comments

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// TODO: Include packages needed for this application
+// Packages required for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown.js");
 
-// TODO: Create an array of questions for user input
+// Array of questions for the README file
 const questions = [
 
 {
@@ -107,25 +107,7 @@ const questions = [
 
 ];
 
-// function retrieveLicense(value) {
-//     if (value === "GNU Affero General Public License v3.0") {
-//         return "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)";
-//     } else if (value === "GNU General Public License v3.0") {
-//         return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
-//     } else if (value === "GNU Lesser General Public License v3.0") {
-//         return "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";
-//     } else if (value === "Apache License 2.0") {
-//         return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-//     } else if (value === "Boost Software License 1.0") {
-//         return "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
-//     } else if (value === "MIT") {
-//         return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-//     } else {
-//         return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
-//     }
-// }
-
-// TODO: Create a function to write README file
+// Function that writes the README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err, data) => {
 		return err ? err : data;
@@ -133,7 +115,10 @@ function writeToFile(fileName, data) {
 }
 
 
-// TODO: Create a function to initialize app
+// Function to initialize the application
+// Waits until all of the questions have been answered
+// the generates a README file based on the format in
+// the generateMarkdown file
 async function init() {
 
 
